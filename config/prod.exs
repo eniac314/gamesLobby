9a47cae9e96c864927374710474c7ad416aa674f
fayong <eniac314@gmail.com>
2018-05-15 20:23:09 +0200

    --

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :games_lobby, GamesLobbyWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :platform, Platform.Repo,
+config :platform, GamesLobby.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
